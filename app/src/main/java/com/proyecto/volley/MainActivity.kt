c8cb158737c3615324cc270ba2a29f51b7717925
Ufoex <leonardo.ulloa@alumno.udg.mx>
2020-12-15 14:28:14 -0600

    Primera subida de proyecto

---
+++ app/src/main/java/com/proyecto/volley/MainActivity.kt
@@ -0,0 +1,66 @@
+package com.proyecto.volley
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.Volley
+import kotlinx.android.synthetic.main.activity_main.*
+import mx.udg.cuvalles.volley.R
+import org.json.JSONException
+
+class MainActivity : AppCompatActivity() {
+
+    var adaptadorPaises: PaisesAdapter?=null
+    var listaPaises:ArrayList<Pais>?=null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+        listaPaises = ArrayList<Pais>()
+
+
+
+        adaptadorPaises = PaisesAdapter(listaPaises!!,this)
+
+        miRecyclerCovid.layoutManager = LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false)
+        miRecyclerCovid.adapter = adaptadorPaises
+
+
+        val queue = Volley.newRequestQueue(this)
+        val url = "https://wuhan-coronavirus-api.laeyoung.endpoint.ainize.ai/jhu-edu/latest"
+
+        val peticionDatosCovid = JsonArrayRequest(Request.Method.GET,url,null, { response ->
+            for(index in 0..response.length()-1){
+                try {
+                    val paisJson = response.getJSONObject(index)
+                    val nombrePais = paisJson.getString("countryregion")
+                    var numeroConfirmados = paisJson.getInt("confirmed")
+                    val numeroMuertos = paisJson.getInt("deaths")
+                    val numeroRecuperados = paisJson.getInt("recovered")
+                    val countryCodeJson = paisJson.getJSONObject("countrycode")
+                    val codigoPais = countryCodeJson.getString("iso2")
+                    //numeroConfirmados = DecimalFormat(numeroConfirmados)
+                    //objeto de kotlin
+                    val paisIndividual = Pais(nombrePais,numeroConfirmados,numeroMuertos,numeroRecuperados,codigoPais)
+                    listaPaises!!.add(paisIndividual)
+                }catch (e:JSONException){
+                    Log.wtf("JsonError",e.localizedMessage)
+                }
+            }
+
+            listaPaises!!.sortByDescending { it.confirmados }
+            adaptadorPaises!!.notifyDataSetChanged()
+        },
+            { error ->
+                Log.e("error_volley",error.localizedMessage)
+            })
+
+        queue.add(peticionDatosCovid)
+
+    }
+}
